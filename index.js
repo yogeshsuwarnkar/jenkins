f19a71f1e71f0094045e85743ba7eccc33344813
yogesh09 <149040839+yogeshsuwarnkar@users.noreply.github.com>
2024-03-01 11:05:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "yogesh suwarnkar" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('changed not ');
+ res.send('changes done');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
